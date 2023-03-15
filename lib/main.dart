455967828246691cd672675b407773bf1c7e1b4c
Abdurrahim <abdurrahimjamalzada@gmail.com>
2023-03-16 00:26:36 +0400

    Get provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:to_do_app/presentation/screens/home/home_screen.dart';
+import 'package:to_do_app/providers/task_provider.dart';
 import 'package:to_do_app/themes/app_themes.dart';
 
 void main() {
@@ -10,11 +12,14 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: AppThemes.appThemes,
-      home: const HomeScreen(),
-      );
+    return ChangeNotifierProvider(
+      create:(context) => TaskProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: AppThemes.appThemes,
+        home: const HomeScreen(),
+        ),
+    );
   }
 }
