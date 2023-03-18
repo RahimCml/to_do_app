5bddacf4da015cff5e5daf760c165d76cd9373a5
Abdurrahim <abdurrahimjamalzada@gmail.com>
2023-03-18 19:57:23 +0400

    Use Consumer in order to rebuild just one widget

--- lib/presentation/screens/home/widgets/floating_action_button.dart
+++ lib/presentation/screens/home/widgets/floating_action_button.dart
@@ -1,25 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:to_do_app/presentation/screens/home/widgets/todo_list_view_widget.dart';
+import 'package:to_do_app/providers/task_provider.dart';
 import '../../viewmodels/check_box_list_item_model.dart';
 
 class FloatingActionBottomWidget extends StatefulWidget {
   const FloatingActionBottomWidget({super.key});
 
   @override
-  State<FloatingActionBottomWidget> createState() => _FloatingActionBottomWidgetState();
+  State<FloatingActionBottomWidget> createState() =>
+      _FloatingActionBottomWidgetState();
 }
 
-class _FloatingActionBottomWidgetState extends State<FloatingActionBottomWidget> {
-   late final TextEditingController titleController;
-   late final TextEditingController subtitleController;
-    List<CheckBoxListItemModel> taskLists = [];
+class _FloatingActionBottomWidgetState
+    extends State<FloatingActionBottomWidget> {
+  late final TextEditingController titleController;
+  late final TextEditingController subtitleController;
+  late final bool isChecked;
 
   @override
   void initState() {
     super.initState();
     titleController = TextEditingController();
     subtitleController = TextEditingController();
-    }
+  }
 
   @override
   void dispose() {
@@ -30,58 +34,63 @@ class _FloatingActionBottomWidgetState extends State<FloatingActionBottomWidget>
 
   void triggerBottomSheet() {
     showModalBottomSheet(
-      context: context,
-      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.vertical(top: Radius.circular(16))),
-      builder: (context) {
-        return ListView(
-          shrinkWrap: true,
-          padding: const EdgeInsets.all(12),
-          children: [
-            TextField(
-              controller: titleController,
-              decoration: const InputDecoration(
-                hintText: 'Enter title'
+        context: context,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(16))),
+        builder: (context) {
+          return ListView(
+            shrinkWrap: true,
+            padding: const EdgeInsets.all(12),
+            children: [
+              TextField(
+                controller: titleController,
+                decoration: const InputDecoration(hintText: 'Enter title'),
               ),
-            ),
-            const SizedBox(height: 16,),
-            TextField(
-              controller: subtitleController,
-              decoration: const InputDecoration(
-                hintText: 'Enter subtitle'
+              const SizedBox(
+                height: 16,
               ),
-            ),
-            const SizedBox(height: 24,),
-            Padding(
-              padding: const EdgeInsets.only(left: 120, right: 120),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
-                  )
+              TextField(
+                controller: subtitleController,
+                decoration: const InputDecoration(hintText: 'Enter subtitle'),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 120, right: 120),
+                child: Consumer<TaskProvider>(
+                  builder: (context, taskProvider, child) {
+                    return ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      )),
+                      onPressed: () {
+                        taskProvider.addTask(CheckBoxListItemModel(
+                            titleController.text, subtitleController.text));
+                        titleController.clear();
+                        subtitleController.clear();
+                        Navigator.pop(context);
+                        setState(() {
+                        });
+                      },
+                      child: const Text('Add'),
+                    );
+                  },
                 ),
-                onPressed: () {
-                  taskLists.add(CheckBoxListItemModel(titleController.text, subtitleController.text));
-                  titleController.clear();
-                  subtitleController.clear();
-                  Navigator.pop(context);
-                  setState(() {
-                  });
-                },
-                child: const Text('Add'),
-              ),
-            )
-          ],
-        );
-      }
-    );
+              )
+            ],
+          );
+        });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: TodoListViewWidget(taskLists: taskLists),
+      body: const TodoListViewWidget(),
       floatingActionButton: FloatingActionButton(
-      onPressed: triggerBottomSheet,
-      child: const Icon(Icons.add_rounded),
+        onPressed: triggerBottomSheet,
+        child: const Icon(Icons.add_rounded),
       ),
     );
   }
--- lib/presentation/screens/viewmodels/check_box_list_item_model.dart
+++ lib/presentation/screens/viewmodels/check_box_list_item_model.dart
@@ -1,6 +1,7 @@
 class CheckBoxListItemModel {
   final String title;
   final String subtitle;
+  late bool isChecked = false;
 
   CheckBoxListItemModel(this.title, this.subtitle);
 }
