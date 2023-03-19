1ead26932464c78726ecbcfbd921645861584562
Abdurrahim <abdurrahimjamalzada@gmail.com>
2023-03-19 20:01:25 +0400

    Use read and watch methods for eading and watching data from provider

--- lib/presentation/screens/home/widgets/check_box_list_item.dart
+++ lib/presentation/screens/home/widgets/check_box_list_item.dart
@@ -1,40 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:to_do_app/providers/task_provider.dart';
 import '../../../../styles/app_text_styles.dart';
 import '../../../../themes/app_colors.dart';
 import '../../viewmodels/check_box_list_item_model.dart';
 
 class CheckBoxListItem extends StatefulWidget {
-  final List<CheckBoxListItemModel> taskLists;
   final CheckBoxListItemModel item;
   final int index;
-  const CheckBoxListItem({super.key, required this.item, required this.index, required this.taskLists});
+  const CheckBoxListItem({
+    super.key,
+    required this.item,
+    required this.index,
+  });
 
   @override
   State<CheckBoxListItem> createState() => _CheckBoxListItemState();
 }
-bool isChecked = false;
 
 class _CheckBoxListItemState extends State<CheckBoxListItem> {
   @override
   Widget build(BuildContext context) {
+    final taskProvider = context.read<TaskProvider>();
     return CheckboxListTile(
-      value: isChecked,
-      controlAffinity: ListTileControlAffinity.leading,
-      secondary: GestureDetector(
-        onTap: () {
-          widget.taskLists.removeAt(widget.index);
-          setState(() {
-          });
-        },
-        child: Icon(Icons.delete_rounded, color: AppColor.trashIconColor,)),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      tileColor: AppColor.tileColor,
-      title: Text(widget.item.title, style: AppTextStyles.lisTileTitleStyle(isChecked),),
-      subtitle: Text(widget.item.subtitle, style: AppTextStyles.lisTileSubtitleStyle(isChecked),),
-      onChanged: (v) {
-        setState(() {
-          isChecked = v!;
+        value: taskProvider.taskList[widget.index].isChecked,
+        controlAffinity: ListTileControlAffinity.leading,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        tileColor: AppColor.tileColor,
+        title: Text(
+          widget.item.title,
+          style: AppTextStyles.lisTileTitleStyle(
+              taskProvider.taskList[widget.index].isChecked),
+        ),
+        subtitle: Text(
+          widget.item.subtitle,
+          style: AppTextStyles.lisTileSubtitleStyle(
+              taskProvider.taskList[widget.index].isChecked),
+        ),
+        onChanged: (value) {
+          taskProvider.taskList[widget.index].isChecked = value!;
+          setState(() {});
         });
-      });
   }
 }
--- lib/presentation/screens/home/widgets/todo_list_view_widget.dart
+++ lib/presentation/screens/home/widgets/todo_list_view_widget.dart
@@ -1,27 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:to_do_app/providers/task_provider.dart';
 
 import '../../viewmodels/check_box_list_item_model.dart';
 import 'check_box_list_item.dart';
 
 class TodoListViewWidget extends StatelessWidget {
-  List<CheckBoxListItemModel> taskLists = [];
-  TodoListViewWidget({
+  const TodoListViewWidget({
     Key? key,
-    required this.taskLists,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final taskProvider = context.watch<TaskProvider>();
     return ListView.separated(
-        padding: const EdgeInsets.all(12),
-        itemCount: taskLists.length,
-        shrinkWrap: true,
-        physics: const ClampingScrollPhysics(),
-        separatorBuilder:(context, index) => const SizedBox(height: 12),
-        itemBuilder:(context, index) {
-          CheckBoxListItemModel item = taskLists[index];
-          return CheckBoxListItem(item: item, taskLists: taskLists, index: index,);
-        },
-      );
+      padding: const EdgeInsets.all(12),
+      itemCount: taskProvider.taskList.length,
+      shrinkWrap: true,
+      physics: const ClampingScrollPhysics(),
+      separatorBuilder: (context, index) => const SizedBox(height: 12),
+      itemBuilder: (context, index) {
+        CheckBoxListItemModel item = taskProvider.taskList[index];
+        return CheckBoxListItem(
+          item: item,
+          index: index,
+        );
+      },
+    );
   }
 }
